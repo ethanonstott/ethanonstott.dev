873dc8f58342b221d331d339815d3b517953bf2c
Ethan Onstott <storm@stormprograms.com>
2020-02-29 21:46:57 -0500

    Add rate limits to prevent DDOS

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require(`express`);
 const path = require(`path`);
+const rateLimiter = require('express-rate-limit');
 const app = express();
 
+// allow 20 requests per minute
+var limiter = new rateLimiter({
+	windowMs: 60000,
+	max: 20
+});
+
 app.use('/static', express.static(__dirname + '/static'));
+app.use(limiter);
 
 app.get('/', (req, res) =>{
-	res.sendFile(path.join(__dirname, '/views/index.html'))
+	res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
 const PORT = process.env.PORT || 8080;
